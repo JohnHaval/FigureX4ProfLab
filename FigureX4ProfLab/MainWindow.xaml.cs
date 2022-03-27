9976b7b1680d4306469c8f155e2fda8fa91e53bb
Hapro Bishop <acfowo45@gmail.com>
2022-03-27 17:49:41 +0300

    ФИКС БАГОВ

--- FigureX4ProfLab/MainWindow.xaml.cs
+++ FigureX4ProfLab/MainWindow.xaml.cs
@@ -29,18 +29,113 @@ namespace FigureX4ProfLab
         private void FindKind_Click(object sender, RoutedEventArgs e)
         {
             KindOfFigure4X kind = new KindOfFigure4X();
-            Result.Text = kind.FindKind(Convert.ToInt32(X1.Text), Convert.ToInt32(Y1.Text), Convert.ToInt32(X2.Text)
-                , Convert.ToInt32(Y2.Text), Convert.ToInt32(X3.Text)
-                , Convert.ToInt32(Y3.Text), Convert.ToInt32(X4.Text),
-                Convert.ToInt32(Y4.Text)).ToString();
+            int x1,y1,x2, y2,x3,y3,x4,y4;
+            try
+            {
+                x1 = Convert.ToInt32(X1.Text);
+            }
+            catch
+            {
+                Message("x1");
+                X1.Focus();
+                return;
+            }
+            try
+            {
+                y1 = Convert.ToInt32(Y1.Text);
+            }
+            catch
+            {
+                Message("y1");
+                Y1.Focus();
+                return;
+            }
+            try
+            {
+                x2 = Convert.ToInt32(X2.Text);
+            }
+            catch
+            {
+                Message("x2");
+                X2.Focus();
+                return;
+            }
+            try
+            {
+                y2 = Convert.ToInt32(Y2.Text);
+            }
+            catch
+            {
+                Message("y2");
+                Y2.Focus();
+                return;
+            }
+            try
+            {
+                x3 = Convert.ToInt32(X3.Text);
+            }
+            catch
+            {
+                Message("x3");
+                X3.Focus();
+                return;
+            }
+            try
+            {
+                y3 = Convert.ToInt32(Y3.Text);
+            }
+            catch
+            {
+                Message("y3");
+                Y3.Focus();
+                return;
+            }
+            try
+            {
+                x4 = Convert.ToInt32(X4.Text);
+            }
+            catch
+            {
+                Message("x4");
+                X4.Focus();
+                return;
+            }
+            try
+            {
+                y4 = Convert.ToInt32(Y4.Text);
+            }
+            catch
+            {
+                Message("y4");
+                Y4.Focus();
+                return;
+            }
+            if (kind.FindKind(x1, y1, x2, y2, x3, y3, x4, y4)) Result.Text = kind.KindOfFigure;
+        }
+        private void Message(string value)
+        {
+            MessageBox.Show($"Неверно введено значение {value}!",
+                "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void Help_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Данная программа имеет следующие особенности:\n" +
-                "1) Возможность ввода количества знаков в поля - 5\n" +
+                "1) Возможность ввода количества знаков в поля - 4\n" +
                 "2) Представлено только меню для выполнения действий, так как не было смысла разрабатывать кнопки среди полей(Для удобного восприятия)\n" +
-                "3) Если у нас не четырехугольник(даже, если не правильно заданы точки - вывод информации об невозможности определения)");
+                "3) Программа предназначена для определения вида ТОЛЬКО четырехугольника", "Справка",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void AboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик программы: Лопаткин Сергей ИСП-31 (GitHub.Name = \"Hapro Bishop\")\n" +
+                "Задание 5.Разработать программу определения вида четырехугольника, " +
+                "заданного координатами вершин на плоскости: " +
+                "квадрат, прямоугольник, параллелограмм, ромб, " +
+                "равнобедренная трапеция, прямоугольная трапеция, " +
+                "трапеция общего вида, четырехугольник общего вида.", "О программе",
+                MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
--- KindOfFigure/KindOfFigure4X.cs
+++ KindOfFigure/KindOfFigure4X.cs
@@ -9,7 +9,20 @@ namespace KindOfFigure
     public class KindOfFigure4X
     {
         int[,] _arr;
+        int _iDiag;//0, если AC(1-2) или BD(3-0) - 1
         public string KindOfFigure { get; set; }
+        /// <summary>
+        /// Метод для нахождения вида четырехугольника по точкам (преобразование в массив и отправка в FindKind с параметром int[,]arr)
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <param name="x3"></param>
+        /// <param name="y3"></param>
+        /// <param name="x4"></param>
+        /// <param name="y4"></param>
+        /// <returns>true - если такой четырехугольник существует и false - если указан не четырехугольник</returns>
         public bool FindKind(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
         {
             int[,] arr = new int[4, 2];
@@ -23,6 +36,11 @@ namespace KindOfFigure
             arr[3, 1] = y4;
             return FindKind(arr);
         }
+        /// <summary>
+        /// Метод нахождения вида четырехугольника
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>true - если такой четырехугольник существует и false - если указан не четырехугольник</returns>
         public bool FindKind(int[,] arr)
         {
             if (arr == null || arr.GetLength(0) <= 3 || arr.GetLength(0) > 4
@@ -44,35 +62,80 @@ namespace KindOfFigure
                 else KindOfFigure = "Ромб";
                 return true;
             }
-            if ((mas[0] == mas[1] && mas[2] == mas[3] && mas[1] != mas[2] && mas[0] != mas[3]) ||
-                (mas[1] == mas[2] && mas[3] == mas[0] && mas[0] != mas[1] && mas[2] != mas[3]))
+            if ((mas[0] == mas[2] && mas[1] == mas[3]) ||
+                (mas[2] == mas[0] && mas[3] == mas[0]))//Сравнение на равность сторон
             {
-                if (EqualDiags()) KindOfFigure = "Прямоугольник";
+                if (EqualDiags()) KindOfFigure = "Прямоугольник";//Сравнение диагоналей и результирование
                 else KindOfFigure = "Параллелограмм";
+                return true;
             }
-            return false;
-        }
+            if (mas[0] != mas[1] && mas[1] != mas[2] && mas[2] != mas[3] && mas[3] != mas[0])
+            {
+                KindOfFigure = "Четырехугольник общего вида";
+                return true;
+            }
+            if (mas[0] == mas[2] ^ mas[1] == mas[3])//Сравнение равность одних из противоположных сторон(исключающе)
+            {
+                KindOfFigure = "Равнобедренная трапеция";
+                return true;
+            }
+            else
+            {
+                double diag = GetBigDiag();//Получение большей диагонали от расчета
+                double provePifagor;
+                if (_iDiag == 0)
+                {
+                    provePifagor = Math.Pow(mas[0], 2) + Math.Pow(mas[3], 2);
+                }
+                else
+                {
+                    provePifagor = Math.Pow(mas[3], 2) + Math.Pow(mas[2], 2);
+                }
+                if (provePifagor == diag)
+                    KindOfFigure = "Прямоугольная трапеция";//Сравнение значения по теореме Пифагора и найденное
+                else KindOfFigure = "Трапеция общего вида"; 
+                return true;
+            }
+        }/// <summary>
+        /// Используется для очистки полей/свойств
+        /// </summary>
         public void Clear()
         {
             KindOfFigure = "";
             _arr = null;
+            _iDiag = 0;
         }
         /// <summary>
         /// Поиск диагоналей
         /// </summary>
-        /// <returns></returns>
+        /// <returns>diags - массив значений 1 диагонали (AC) и 2 (BD)</returns>
         private double[] FindDiag()
         {
             double[] diags = new double[2];
             diags[0] = Math.Sqrt(Math.Pow(_arr[2, 0] - _arr[0, 0], 2) + Math.Pow(_arr[2, 1] - _arr[0, 1], 2));
             diags[1] = Math.Sqrt(Math.Pow(_arr[3, 0] - _arr[1, 0], 2) + Math.Pow(_arr[3, 1] - _arr[1, 1], 2));
             return diags;
-        }
+        }/// <summary>
+        /// Используется для сравнения диагоналей
+        /// </summary>
+        /// <returns>Возвращает true, если равны и false - иначе</returns>
         private bool EqualDiags()
         {
             double[] diags = FindDiag();
             if (diags[0] == diags[1]) return true;
             return false;
+        }/// <summary>
+        /// Используется внутри класса для получения значения большей диагонали
+        /// </summary>
+        /// <returns></returns>
+        private double GetBigDiag()
+        {
+            double[] diags = FindDiag();
+            if (diags[0] > diags[1])
+            {
+                return diags[_iDiag = 0];
+            }
+            else return diags[_iDiag = 1];
         }
     }
 }
